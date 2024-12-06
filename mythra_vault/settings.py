073238c925ac27d69400ee65c9a74f8d43ce1d77
Abdul Samee <sameeabdul56@gmail.com>
2024-12-06 11:05:54 -0500

    Preparing for heroku

--- mythra_vault/settings.py
+++ mythra_vault/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-vaeqo0iix8-g^(*mu@4($)0&cyp+e$&pz*2rj+3y1c(^8pq9k_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
